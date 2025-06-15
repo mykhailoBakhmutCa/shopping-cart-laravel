18d8d103763d5b417824d4ba44a4c8509852702c
mykhailo.bakhmut <mykhailo.bakhmut.ca@gmail.com>
2025-06-15 15:59:25 -0400

    minimal data view

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,16 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shopping-cart-laravel</title>
 </head>
+
 <body>
-    <h1>Init</h1>
-    <ul>
-        @foreach ($cartItems as $cartItem)
-            <li>{{$cartItem->product_name}}</li>
-        @endforeach
-    </ul>
+    <div class="container">
+        <div>
+            @foreach ($cartItems as $cartItem)
+                <p>product name:{{ $cartItem->product_name }}</p>
+                <p>product price:{{ number_format($cartItem->price, 2) }}</p>
+                <label for="quantity-{{ $cartItem->id }}">product quantity:</label>
+                <input type="number" name="quantity-{{ $cartItem->id }}" id="quantity-{{ $cartItem->id }}"
+                    value="{{ $cartItem->quantity }}" min=1 max=50>
+                <p>product total price:{{ $cartItem->total_price }}</p>
+                <br>
+            @endforeach
+        </div>
+        <div>
+            <p>Subtotal: {{ $totals['subtotal'] }}</p>
+            <p>GST: {{ $totals['gst'] }} - {{ Config::get('taxes.gst') * 100 }}%</p>
+            <p>QST: {{ $totals['qst'] }} - {{ Config::get('taxes.qst') * 100 }}%</p>
+            <p>Total: {{ $totals['total'] }}</p>
+        </div>
+    </div>
 </body>
+
 </html>
