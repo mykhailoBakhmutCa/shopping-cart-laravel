607a2caca9882679c0dec1f7491196428eed9571
mykhailo.bakhmut <mykhailo.bakhmut.ca@gmail.com>
2025-06-15 19:54:33 -0400

    cart blade added logic for ajax update data

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shopping-cart-laravel</title>
+    <meta name='csrf-token' content="{{ csrf_token() }}">
 </head>
 
 <body>
@@ -17,15 +18,15 @@
                 <input type="number" name="quantity-{{ $cartItem->id }}" id="quantity-{{ $cartItem->id }}"
                     value="{{ $cartItem->quantity }}" min=1 max=50 data-item-id={{ $cartItem->id }}
                     class="quantity-input">
-                <p>product total price:{{ $cartItem->total_price }}</p>
+                <p>product total price:{{ $cartItem->total_price_formatted }}</p>
                 <br>
             @endforeach
         </div>
         <div>
-            <p>Subtotal: {{ $totals['subtotal'] }}</p>
-            <p>GST: {{ $totals['gst'] }} - {{ Config::get('taxes.gst') * 100 }}%</p>
-            <p>QST: {{ $totals['qst'] }} - {{ Config::get('taxes.qst') * 100 }}%</p>
-            <p>Total: {{ $totals['total'] }}</p>
+            <p>Subtotal: <span id="subtotal">{{ $totals['subtotal'] }}</span></p>
+            <p>GST: <span id="gst">{{ $totals['gst'] }}</span> - {{ Config::get('taxes.gst') * 100 }}%</p>
+            <p>QST: <span id="qst">{{ $totals['qst'] }}</span> - {{ Config::get('taxes.qst') * 100 }}%</p>
+            <p>Total: <span id="total">{{ $totals['total'] }}</span></p>
         </div>
     </div>
 
