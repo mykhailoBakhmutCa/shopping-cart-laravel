008ad9d89234f6bbb2c3e1296db0e8dfd5f4c7c7
mykhailo.bakhmut <mykhailo.bakhmut.ca@gmail.com>
2025-06-15 18:01:39 -0400

    added route cart.update-quantity

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\CartController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [CartController::class, 'index'])->name('cart.index');
+Route::post('/cart/update-quantity', [CartController::class, 'updateQuantity'])->name('cart.update-quantity');
